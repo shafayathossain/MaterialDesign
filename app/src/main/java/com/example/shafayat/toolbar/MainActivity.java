6d58ce1abc8188cead9aa11aa820206edc2e907d
Shafayat <shafayathossainkhan@gmail.com>
2017-07-30 07:51:48 +0600

    pop up added and popup color changed

--- app/src/main/java/com/example/shafayat/toolbar/MainActivity.java
+++ app/src/main/java/com/example/shafayat/toolbar/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.shafayat.toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if(i == R.id.item1){
+            Toast.makeText(this, "item one selected", Toast.LENGTH_SHORT).show();
+        }
+        else if(i == R.id.item2){
+            Toast.makeText(this, "item two selected", Toast.LENGTH_SHORT).show();
+        }
+        else if(i == R.id.item3){
+            Toast.makeText(this, "item 3 selected", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
